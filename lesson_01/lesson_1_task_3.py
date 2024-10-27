b665785f8cc905ab4238ccc8d9a2a42301a3eb26
Natalya87-ux <kagome_07@mai.ru>
2024-10-27 13:28:03 +0300

    HW Lesson1

---
+++ lesson_01/lesson_1_task_3.py
@@ -0,0 +1,3 @@
+first_name = input("Наталья ")
+last_name = input("Елизавета")
+print("Вас зовут:",first_name,last_name)
---
+++ lesson_01/lesson_1_task_5.py
@@ -0,0 +1,13 @@
+def print_parameter(num):
+    print(num)
+print_parameter(8)
+print_parameter(8)
+print_parameter(0)
+print_parameter(0)
+print_parameter(5)
+print_parameter(5)
+print_parameter(5)
+print_parameter(3)
+print_parameter(5)
+print_parameter(3)
+print_parameter(5)
