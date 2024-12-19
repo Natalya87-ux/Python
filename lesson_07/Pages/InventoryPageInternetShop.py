3d4182c7b55ee7073150152dc4c7d36972e65c1b
Natalya87-ux <kagome_07@mai.ru>
2024-12-19 21:34:42 +0300

    Lesson7Remarks

--- lesson_07/Pages/InventoryPageInternetShop.py
+++ lesson_07/Pages/InventoryPageInternetShop.py
@@ -2,32 +2,35 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+
 class InventoryPageInternetShop:
-    def __init__(self,driver):
+    def __init__(self, driver):
         self._driver = driver
         self._driver.get("https://www.saucedemo.com/inventory.html")
         self._driver.implicitly_wait(10)
         self._driver.maximize_window()
-        
 
     def set_cookie_policy(self):
-        cookie = {
-            "name": "cookie_policy",
-            "value": "1"
-        }
+        cookie = {"name": "cookie_policy", "value": "1"}
         self._driver.add_cookie(cookie)
 
     def wait(self):
         WebDriverWait(self._driver, 10).until(
-        EC.presence_of_all_elements_located((By.ID, "add-to-cart-test.allthethings()-t-shirt-(red)"))
-    )
-        
+            EC.presence_of_all_elements_located(
+                (By.ID, "add-to-cart-test.allthethings()-t-shirt-(red)")
+            )
+        )
+
     def additemintobasket(self):
-        additem = self._driver.find_element(By.ID,"add-to-cart-sauce-labs-backpack")
+        ID = "add-to-cart-sauce-labs-backpack"
+        additem = self._driver.find_element(By.ID, ID)
         additem.click()
-        additem = self._driver.find_element(By.ID,"add-to-cart-sauce-labs-bolt-t-shirt")
+        ID = "add-to-cart-sauce-labs-bolt-t-shirt"
+        additem = self._driver.find_element(By.ID, ID)
         additem.click()
-        additem = self._driver.find_element(By.ID,"add-to-cart-sauce-labs-onesie")
+        ID = "add-to-cart-sauce-labs-onesie"
+        additem = self._driver.find_element(By.ID, ID)
         additem.click()
-        additem = self._driver.find_element(By.CLASS_NAME,"shopping_cart_link")
+        ID = "shopping_cart_link"
+        additem = self._driver.find_element(By.CLASS_NAME, ID)
         additem.click()
--- lesson_07/Pages/MainPageСalculator.py
+++ lesson_07/Pages/MainPageСalculator.py
@@ -3,47 +3,41 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 
-class MainPageСalculator:
-    def __init__(self,driver):
+class MainPageCalculator:
+    def __init__(self, driver):
         self._driver = driver
-        self._driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+        self._driver.get
+        ("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
         self._driver.implicitly_wait(10)
         self._driver.maximize_window()
-        
 
     def set_cookie_policy(self):
-        cookie = {
-            "name": "cookie_policy",
-            "value": "1"
-        }
+        cookie = {"name": "cookie_policy", "value": "1"}
         self._driver.add_cookie(cookie)
-        
 
-    def wait(self):
+    def wait_for_elements(self):
         WebDriverWait(self._driver, 25).until(
-        EC.presence_of_all_elements_located((By.ID, "delay"))
+            EC.presence_of_all_elements_located((By.ID, "delay"))
         )
-        
 
     def enter_time_delay(self, delay):
-        self._driver.find_element(By.ID,'delay').clear()
-        input_field = self._driver.find_element(By.ID,'delay')
+        input_field = self._driver.find_element(By.ID, "delay")
+        input_field.clear()
         input_field.send_keys(delay)
-        
 
     def enter_expression(self):
-        keynums = self._driver.find_element(By.XPATH,"//span[contains(.,'7')]")
-        keynums.click()
-        keynums = self._driver.find_element(By.XPATH,"//span[contains(.,'+')]")
-        keynums.click()
-        keynums = self._driver.find_element(By.XPATH,"//span[contains(.,'8')]")
-        keynums.click()
-        keynums = self._driver.find_element(By.XPATH,"//span[contains(.,'=')]")
-        keynums.click()
-        
-    
-    def check_result(self):
-        WebDriverWait(self._driver, 45).until(EC.text_to_be_present_in_element((By.CLASS_NAME, "screen"),'15'))
-        t = self._driver.find_element(By.CLASS_NAME,'screen')
-        assert t.text == '15', 'ОШИБКА?!'
-        
+        self._click_button("7")
+        self._click_button("+")
+        self._click_button("8")
+        self._click_button("=")
+
+    def _click_button(self, button_text):
+        button = self._driver.find_element
+        (By.XPATH, f"//span[contains(.,'{button_text}')]")
+        button.click()
+
+    def get_result(self):
+        WebDriverWait(self._driver, 46).until(
+            EC.text_to_be_present_in_element((By.CLASS_NAME, "screen"), "15")
+        )
+        return self._driver.find_element(By.CLASS_NAME, "screen").text
